6456ac257deb2ee7db7adc83fd08759e2a24a912
Adnan-shamsi <61737425+Adnan-shamsi@users.noreply.github.com>
2020-07-19 19:26:17 +0530

    Update start.cpp

--- start.cpp
+++ start.cpp
@@ -2,26 +2,26 @@
 #include <ext/pb_ds/assoc_container.hpp> 
 #include <ext/pb_ds/tree_policy.hpp> 
 #include <functional> 
-#define MOD  (int)(1e9+7)
-#define SIZE (int)(1e5+5)
-#define all(x) x.begin(),x.end()
-#define lb     lower_bound
-#define ub     upper_bound
-#define pb     push_back
-#define F      first
-#define S      second
-#define cnt_set1(x) __builtin_popcountll(x)  
-#define trailing_0(x) __builtin_ctz(x)
-#define PI (double) acos(-1) 
-#define prDouble(x) cout<<fixed<<setprecision(10)<<x;
-#define IOfast ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define MOD                   (int)(1e9+7)
+#define SIZE                  (int)(1e5+5)
+#define all(x)                x.begin(),x.end()
+#define r_all(x)              x.rbegin(),x.rend()
+#define lb                    lower_bound
+#define ub                    upper_bound
+#define pb                    push_back
+#define F                     first
+#define S                     second
+#define cnt_set1(x)           __builtin_popcountll(x)  
+#define trailing_0(x)         __builtin_ctz(x)
+#define PI                    (double)acos(-1) 
+#define prDouble(x)           cout<<fixed<<setprecision(10)<<x;
+#define IOfast                ios_base::sync_with_stdio(false); cin.tie(NULL);
 #define get_element_by_key(x) find_by_order(x) 
-#define get_key(x)   order_of_key(x)
-#define debug(x) cout<<#x<<" = "<<x<<"\n";
-#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+#define get_key(x)            order_of_key(x)
+#define debug(x)              cout<<#x<<" = "<<x<<"\n";
+#define error(args...)        { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
 using namespace std;
 using namespace __gnu_pbds; 
-//less_int<int>
 
 typedef tree<int64_t, null_type,less<int64_t>, rb_tree_tag, tree_order_statistics_node_update> indexed_set;
 
@@ -37,48 +37,30 @@ void err(istream_iterator<string> it, T a, Args... args){
 }
 
 ///pair cout
-template<typename T,typename G> print(pair<T,G> p){ cout<<"{"<<p.F<<","<<p.S<<"},";  }
+template<typename T,typename G> void print(pair<T,G> p){ cout<<"{"<<p.F<<","<<p.S<<"},";  }
 ///vector cout 
-template<typename T> print(vector <T> v){ for(auto item:v)cout<<item<<" ";cout<<"\n";} 
+template<typename T> void print(vector <T> v){ for(auto item:v)cout<<item<<" ";cout<<"\n";} 
 ///set cout
-template<typename T> print(set <T> v){ for(auto item:v)cout<<item<<" ";cout<<"\n";} 
+template<typename T> void print(set <T> v){ for(auto item:v)cout<<item<<" ";cout<<"\n";} 
 ///multiset cout
-template<typename T> print(multiset <T> v){ for(auto item:v)cout<<item<<" ";cout<<"\n";} 
+template<typename T> void print(multiset <T> v){ for(auto item:v)cout<<item<<" ";cout<<"\n";} 
 //map cout
-template<typename T,typename G> print(map <T,G> v){ for(auto item:v) cout<<"{"<<item.first<<"=>"<<item.second<<"},"<<" ";cout<<"\n";} 
+template<typename T,typename G> void print(map <T,G> v){ for(auto item:v) cout<<"{"<<item.first<<"=>"<<item.second<<"},"<<" ";cout<<"\n";} 
 
-//dsu struct 1 is the starting
-struct dsu_struct
+//with mod
+int64_t power(int64_t base,int exp,int64_t mod)
 {
-  vector<int> par;
-  int total_comp;
-
-  void init(int n)
-  { //index 1 is the starting 
-    total_comp = n;
-    par.resize(n+1);
-    for(int i=1;i<=n;i++) par[i] = i;
-  }
-  
-  int get_super_parent(int x)
-  {
-    if(par[x] == x) return x;
-    else return par[x] = get_super_parent(par[x]); 
-  }
-
-  void merge(int x,int y)
-  {
-    int super_par_x = get_super_parent(x);
-    int super_par_y = get_super_parent(y);
+    if (exp == 0) return 1;
+    if (exp == 1) return base % mod;
     
-    if(super_par_x != super_par_y)
-    {
-      par[super_par_x]=super_par_y;
-      total_comp--;
-    }
-  }
-}; 
-
+    int64_t t = power(base, exp / 2,mod) % mod;
+    t = (t * t) % mod;
+    if (exp % 2 == 0)
+     return t;
+    else
+     return (base * t) % mod;
+}
+//without mod
 int64_t power(int64_t base,int exp)
 {
     if (exp == 0) return 1;
@@ -89,7 +71,7 @@ int64_t power(int64_t base,int exp)
     if (exp % 2 == 0)
      return t;
     else
-     return base * t;
+     return (base * t);
 }
 
 //void seiveErath(int n)
@@ -109,9 +91,13 @@ vector<int64_t> seiveErath(int n)
   return prime;    
 }
 
-//int64_t dp[(int)4e8];
+//int64_t dp[(int)1e5+10][2]; 
 //int visited[(int)2e5 + 1];
 //vector <int> graph[(int)2e5 + 1];
+
+void solve()
+{
+}
 
 int main()
 {
@@ -127,10 +113,16 @@ int main()
   //memset(dp,-1,sizeof(dp));
   while(q--)
   {
-
+    solve();
   }
+
+  //REMEMBER TO REMOVE ALL error() 
+  //BECAUSE IT PASSES CODEFORCES JUDGE BUT 
+  //IT SLOWS DOWN CODE TREMENDOUSLY AND CAN CAUSE TLE
+  //REMEMBER ONCE 77MS CODE BECAME 865MS CODE ON ADDING error()  
+  
   #ifndef ONLINE_JUDGE
-  cout << "\nTimes Elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " sec\n";
+   cout << "\nTimes Elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " sec\n";
   #endif  
   
   return 0;
