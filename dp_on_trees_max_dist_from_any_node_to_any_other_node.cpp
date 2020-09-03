e86ead06f3aa4f85df3f834c1a5bc6156ea56e90
Adnan-shamsi <61737425+Adnan-shamsi@users.noreply.github.com>
2020-09-03 21:13:00 +0530

    Update dp_on_trees_max_dist_from_any_node_to_any_other_node.cpp

--- dp_on_trees_max_dist_from_any_node_to_any_other_node.cpp
+++ dp_on_trees_max_dist_from_any_node_to_any_other_node.cpp
@@ -1,107 +1,14 @@
-#include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp> 
-#include <ext/pb_ds/tree_policy.hpp> 
-#include <functional> 
-#define MOD                   (int)(1e9+7)
-#define all(x)                x.begin(),x.end()
-#define r_all(x)              x.rbegin(),x.rend()
-#define lb                    lower_bound
-#define ub                    upper_bound
-#define pb                    push_back
-#define F                     first
-#define S                     second
-#define cnt_set1(x)           __builtin_popcountll(x)  
-#define trailing_0(x)         __builtin_ctz(x)
-#define PI                    (double)acos(-1) 
-#define prDouble(x)           cout<<fixed<<setprecision(6)<<x;
-#define IOfast                ios_base::sync_with_stdio(false); cin.tie(NULL);
-#define get_element_by_index(x) find_by_order(x) 
-#define get_index(x)            order_of_key(x)
-#define debug(x)              cout<<#x<<" = "<<x<<"\n";
-#define error(args...)        { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
-using namespace std;
-using namespace __gnu_pbds; 
- 
-typedef tree<int64_t, null_type,less<int64_t>, rb_tree_tag, tree_order_statistics_node_update> indexed_set;
- 
-typedef tree<int64_t, null_type, less_equal<int64_t>, rb_tree_tag,tree_order_statistics_node_update> indexed_multiset;
-//find_by_order(x) -> returns an iterator to the element at a given position
-//order_of_key(x) -> returns the position of a given element
- 
-void err(istream_iterator<string> it) {}
-template<typename T, typename... Args>
-void err(istream_iterator<string> it, T a, Args... args){
-  cerr << *it << " = " << a << endl;
-  err(++it, args...);
-}
- 
-///pair cout
-template<typename T,typename G> void print(pair<T,G> p){ cout<<"{"<<p.F<<","<<p.S<<"},";  }
-///vector cout 
-template<typename T> void print(vector <T> v){ for(auto item:v)cout<<item<<" ";cout<<"\n";} 
-///set cout
-template<typename T> void print(set <T> v){ for(auto item:v)cout<<item<<" ";cout<<"\n";} 
-///multiset cout
-template<typename T> void print(multiset <T> v){ for(auto item:v)cout<<item<<" ";cout<<"\n";} 
-//map cout
-template<typename T,typename G> void print(map <T,G> v){ for(auto item:v) cout<<"{"<<item.first<<"=>"<<item.second<<"},"<<" ";cout<<"\n";} 
-
-//with mod
-int64_t power(int64_t base,int exp,int64_t mod)
-{
-    if (exp == 0) return 1;
-    if (exp == 1) return base % mod;
-    
-    int64_t t = power(base, exp / 2,mod) % mod;
-    t = (t * t) % mod;
-    if (exp % 2 == 0)
-     return t;
-    else
-     return (base * t) % mod;
-}
-//without mod
-int64_t power(int64_t base,int exp)
-{
-    if (exp == 0) return 1;
-    if (exp == 1) return base;
-    
-    int64_t t = power(base, exp / 2);
-    t = (t * t);
-    if (exp % 2 == 0)
-     return t;
-    else
-     return (base * t);
-}
-
-//void seiveErath(int n)
-vector<int> seiveErath(int64_t n)
-{
-  // if n > 4e5 make prime then make prime and mark global 
-  //change the type to void
-  vector<int> prime; 
-  bool mark[n+1]={0}; //primes as 0 and non primes as 1
-  for(int64_t i=2;i<=n;i++)
-  {
-    if(!mark[i])
-    { 
-      prime.pb(i);
-      int64_t temp=i*i;
-      while(temp<=n){ mark[temp] = 1;temp+=i;} 
-    }  
-  }
-  return prime;    
-}
-
-
-//int64_t dp[501][501*(500)/2 + 1];
-//int visited[(int)2e5 + 1];
 vector <int> graph[(int)2e5 + 1];
 
+//in array is for subgraph , whereas out is for parent  
 int in[(int)2e5 + 1] , out[(int)2e5 + 1];
 
 //in array construction
+//for lower sub tree
+//building upwards
 void dfs1(int node , int par)
 {
+  //initializing in[node] by zero 
   in[node] = 0;
   for(auto child:graph[node])
   {
@@ -113,16 +20,26 @@ void dfs1(int node , int par)
 }
 
 //out array construction
+//out can be by any one either branch of parent or parent of parent
+
 void dfs2(int node , int par)
 {
   //we have to initialize by -1 not zero because below we are adding 1 to mxs , 
   //hence -1 nullifies the effect  
+  
+  //we are taking mx1 , mx2 because maximum of branches of parnent 
+  //we need 2 mxs because it may be the case that in of parent has been constructed by than subtree only
+  //hence at that time we need mx2
   int mx1 = -1 , mx2 = -1;
   
+  //rectifying values of mx1 , mx2
   for(int child:graph[node])
   {
     if(child == par)
       continue;
+    
+    //if mx1 , mx2
+    //editing mxs values
     if(in[child] >= mx1)
     {
       mx2 = mx1;
@@ -131,7 +48,8 @@ void dfs2(int node , int par)
     else if(in[child] > mx2)
       mx2 = in[child];   
   }
-
+  
+  //initializing out value of child before going into it
   for(int child:graph[node])
   {
     if(child == par)
@@ -141,18 +59,24 @@ void dfs2(int node , int par)
     //we have to neglect the effect if it is from the same subtree
     if(in[child] == mx1)
       longest = mx2;
-
-    out[child] = 1 + max(out[node] ,1 + longest);
+    //1 + longest is because we have to move from branch to parent whose value we have taken
+    //1 + max(..) is because from parent of current child we are moving to current child 
+    out[child] = 1 + max(out[node] , 1 + longest);
+    
+   //since we have initialize out[child] we can go to current child itself 
     dfs2(child , node); 
   }
 }
 
 void solve()
 {
+  //n->tree nodes
   int n;
   cin>>n;
+  //m->edges
   int m = n-1;
   
+  //taking edges
   while(m--)
   {
     int u , v;
@@ -167,30 +91,16 @@ void solve()
   //out array construction
   dfs2(1 , 0); 
   
+ //since we have calculated in and out array correctly we can get max in 0(1); 
+ 
+  //max_distance from any node to any other node  
   for(int i = 1;i<=n;i++)
      cout<<max(in[i] , out[i])<<" ";
 }
 
 int main()
 {
-  IOfast;
-  #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout); 
-  #endif   
-
-  int q=1;
-  //preCalc();
-  //seiveErath((int64_t)1e6 + 1);
-  //cin>>q;
-  //memset(dp,-1,sizeof(dp));
-  while(q--)
-  {
-    solve();
-  }
-  #ifndef ONLINE_JUDGE
-  cout << "\nTimes Elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " sec\n";
-  #endif  
+  solve();
   
   return 0;
 }
