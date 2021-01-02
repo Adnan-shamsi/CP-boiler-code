19d938ae185890415473acc3881a1a6881f45e66
Adnan-shamsi <61737425+Adnan-shamsi@users.noreply.github.com>
2021-01-02 23:15:34 +0530

    Update nCr_with _MOD_prime.cpp

--- nCr_with _MOD_prime.cpp
+++ nCr_with _MOD_prime.cpp
@@ -7,12 +7,11 @@ void preCalc()  //precalculation for factorial
      fact[i] = (fact[i - 1] * i) % MOD;
 }
 
-void solve()
+int64_t get_nCr(int64_t n,int64_t r)
 {
-  int n , r;
-  cin>>n>>r;
   int64_t den = (fact[r] * fact[n - r]) % MOD; // n!/(r!(n-r)!)   , den = denominator , we we can multipy 
-  int64_t den_inverse = power(den,MOD-2,MOD); //using Fermat's Little theorem if MOD is prime  
-  int64_t nCr = (fact[n] * den_inverse) % MOD;  //since we are applying Modular nCr we have to use modular inverse of denominator
-                                              // and multiply it with numerator then take mod of that multiplication;
+  int64_t den_inverse = power(den,MOD-2,MOD);  //using Fermat's Little theorem if MOD is prime  
+  int64_t nCr = (fact[n] * den_inverse) % MOD; //since we are applying Modular nCr we have to use modular inverse of denominator
+                                               // and multiply it with numerator then take mod of that multiplication;
+  return nCr;
 }
